88c1486e03e6ee3db9fe7942170975463c160a84
Fernanda Fernandez <ffernandamarisol@gmail.com>
2021-04-16 10:43:00 -0300

    new design + responsive styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,113 +1,167 @@
 <template>
-  <section>
-    <section
-      class="contact-us-section"
-      :style="{
-        backgroundImage:
-          'url(' + require('@/assets/contact-background.webp') + ')',
-      }"
-    >
-      <v-row justify="center" class="py-0 fill-height">
-        <v-col
-          sm="6"
-          md="4"
-          lg="4"
-          xl="4"
-          class="d-flex align-xs-end align-sm-end align-md-end align-lg-start align-xl-start"
-        >
-          <v-img
-            src="@/assets/logo.png"
-            max-height="190"
-            max-width="350"
-            class="mt-10"
-          />
-        </v-col>
-        <v-col
-          xs="8"
-          sm="10"
-          md="10"
-          lg="4"
-          xl="4"
-          class="d-flex align-xs-start align-sm-start align-md-start align-lg-center align-xl-center"
-        >
-          <v-card class="pa-10" style="width: 100%">
-            <div class="text-h2 yellow--text text--darken-3">Get in Touch!</div>
-            <v-spacer class="pt-5" />
-            <v-form
-              name="contact-us-form"
-              ref="form"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              v-model="valid"
-              :lazy-validation="lazy"
-              @submit.prevent="handleSubmit"
+  <section class="section-background">
+    <v-row justify="center" class="pa-5 fill-height">
+      <!-- logo toucan -->
+      <v-col
+        xs="10"
+        sm="10"
+        md="6"
+        lg="6"
+        xl="6"
+        class="d-flex align-xs-end align-sm-end align-md-end align-lg-center align-xl-center"
+      >
+        <v-row justify="center">
+          <v-col>
+            <!-- logo toucan -->
+            <v-row justify="center">
+              <v-img src="@/assets/logo.png" class="mt-10 logo-size" />
+            </v-row>
+
+            <!-- spacer xl only -->
+            <v-spacer v-show="$vuetify.breakpoint.xl" style="padding: 300px" />
+            <!-- spacer lg only -->
+            <v-spacer v-show="$vuetify.breakpoint.lg" style="padding: 150px" />
+          </v-col>
+        </v-row>
+      </v-col>
+      <!-- contact form -->
+      <v-col
+        xs="8"
+        sm="10"
+        md="10"
+        lg="5"
+        xl="5"
+        class="d-flex align-xs-start align-sm-start align-md-start align-lg-center align-xl-center"
+      >
+        <v-row class="fill-height">
+          <v-col
+            class="d-flex align-content-sm-start align-content-md-space-between align-content-lg-center align-content-xl-space-between flex-wrap"
+          >
+            <!-- empty div -->
+            <div style="height: 10px; width: 100%" class="hidden-sm-only"></div>
+            <!-- form box -->
+            <div
+              style="
+                width: 100%;
+                background-color: white;
+                border: 2px solid #ed8023;
+                border-radius: 17px;
+                box-shadow: 20px 20px #ed8023;
+                padding: 60px 60px 40px 60px;
+                height: min-content;
+              "
             >
-              <!-- hidden input for netlify -->
-              <input type="hidden" name="form-name" value="contact-us-form" />
-
-              <!-- Client Full Name -->
-              <div class="label-contact-us">Your Full Name:</div>
-              <v-text-field
-                name="fullName"
-                v-model="fullName"
-                autocomplete="off1"
-                :rules="[rules.required, rules.alphabeticOnly, rules.max60]"
-                color="#F79D20"
-                outlined
-                dense
-              />
-
-              <!-- Company Name -->
-              <div class="label-contact-us">Your Company:</div>
-              <v-text-field
-                name="companyName"
-                v-model="companyName"
-                autocomplete="off1"
-                :rules="[rules.required, rules.max100]"
-                color="#F79D20"
-                outlined
-                dense
-              />
-
-              <!-- Client Email -->
-              <div class="label-contact-us">Your email:</div>
-              <v-text-field
-                name="clientMail"
-                v-model="clientMail"
-                autocomplete="off1"
-                :rules="[rules.required, rules.email, rules.max100]"
-                color="#F79D20"
-                outlined
-                dense
-              />
-
-              <!-- Mail Text -->
-              <div class="label-contact-us">Tell us what you need:</div>
-              <v-textarea
-                name="mailBody"
-                v-model="mailBody"
-                autocomplete="off1"
-                :rules="[rules.required, rules.max255]"
-                color="#F79D20"
-                outlined
-                dense
-              />
-
-              <v-btn
-                type="submit"
-                class="text-capitalize"
-                color="#ED8023"
-                width="130"
-                style="border-radius: 20px; height: 35px; color: white"
+              <div class="text-h2 yellow--text text--darken-3 text-center">
+                Get in Touch!
+              </div>
+              <v-spacer class="pt-5" />
+              <v-form
+                name="contact-us-form"
+                ref="form"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                v-model="valid"
+                :lazy-validation="lazy"
+                @submit.prevent="handleSubmit"
               >
-                Send
-              </v-btn>
-            </v-form>
-          </v-card>
-        </v-col>
-        <v-col cols="4" lg="4" xl="4" class="hidden-md-and-down"> </v-col>
-      </v-row>
-    </section>
+                <!-- hidden input for netlify -->
+                <input type="hidden" name="form-name" value="contact-us-form" />
+
+                <!-- Client Full Name -->
+                <v-text-field
+                  name="fullName"
+                  v-model="fullName"
+                  label="Your Full Name:"
+                  autocomplete="off1"
+                  :rules="[rules.required, rules.alphabeticOnly, rules.max60]"
+                  color="#F79D20"
+                  outlined
+                  dense
+                />
+
+                <!-- Company Name -->
+                <v-text-field
+                  name="companyName"
+                  v-model="companyName"
+                  label="Your Company:"
+                  autocomplete="off1"
+                  :rules="[rules.required, rules.max100]"
+                  color="#F79D20"
+                  outlined
+                  dense
+                />
+
+                <!-- Client Email -->
+                <v-text-field
+                  name="clientMail"
+                  v-model="clientMail"
+                  label="Your email:"
+                  autocomplete="off1"
+                  :rules="[rules.required, rules.email, rules.max100]"
+                  color="#F79D20"
+                  outlined
+                  dense
+                />
+
+                <!-- Mail Text -->
+                <v-textarea
+                  name="mailBody"
+                  v-model="mailBody"
+                  label="Tell us what you need:"
+                  autocomplete="off1"
+                  :rules="[rules.required, rules.max255]"
+                  color="#F79D20"
+                  outlined
+                  dense
+                />
+
+                <v-row justify="center">
+                  <v-btn
+                    type="submit"
+                    class="text-capitalize"
+                    color="#ED8023"
+                    width="130"
+                    depressed
+                    style="
+                      border-radius: 10px;
+                      border: 2px solid #fda63f;
+                      height: 35px;
+                      color: white;
+                    "
+                  >
+                    Send
+                  </v-btn>
+                </v-row>
+              </v-form>
+            </div>
+            <!-- anteater -->
+            <v-row
+              justify="center"
+              class="mt-15 hidden-lg-only hidden-sm-and-down"
+            >
+              <v-col align-self="end">
+                <!-- spacer md only -->
+                <v-spacer
+                  v-show="$vuetify.breakpoint.md"
+                  style="padding: 100px"
+                />
+                <!-- anteater -->
+                <v-row justify="center">
+                  <v-img
+                    src="@/assets/anteater.webp"
+                    max-height="390"
+                    max-width="500"
+                    class="mt-10"
+                  />
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-col>
+      <!-- empty column - only lg, xl -->
+      <v-col lg="1" xl="1" class="hidden-md-and-down"> </v-col>
+    </v-row>
   </section>
 </template>
 
@@ -200,10 +254,37 @@ export default class Home extends Vue {
 }
 </script>
 
-<style scoped>
-.contact-us-section {
-  background-size: cover;
-  height: 101vh;
-  width: 100%;
+<style scoped type="scss">
+::v-deep .v-text-field--outlined .v-label {
+  color: #f9a825;
+}
+.v-text-field--outlined >>> fieldset {
+  border-color: #d8d8d8;
+  border-radius: 10px;
+}
+@media (max-width: 599px) {
+  .section-background {
+    background-color: #ed8023;
+    background-size: cover;
+    height: 120vh;
+    width: 100%;
+  }
+  .logo-size {
+    max-height: 90px;
+    max-width: 150px;
+  }
+}
+@media (min-width: 600px) {
+  .section-background {
+    background-image: url("../assets/contact-background.webp");
+    background-size: cover;
+    height: 102vh;
+    width: 100%;
+  }
+
+  .logo-size {
+    max-height: 390px;
+    max-width: 500px;
+  }
 }
 </style>
